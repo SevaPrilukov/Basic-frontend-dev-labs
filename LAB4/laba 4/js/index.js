5815b7a609507c4089753410ea5381a2d8877bd3
Seva <sevaprilukov@mail.ru>
2023-04-28 18:29:39 +0300

    seva

--- LAB4/laba 4/js/index.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener('DOMContentLoaded', function () {
-    document.querySelector('#burger').addEventListener('click', function () {
-        document.querySelector('#menu').classList.toggle('is-active')
-    
-    })
-    document.querySelector('#burger-close').addEventListener('click', function () {
-        document.querySelector('#menu').classList.toggle('is-active')
-    })
-  })
