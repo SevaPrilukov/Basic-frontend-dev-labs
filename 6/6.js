bec67794010893beaef1136490a1c9d7574c617c
Seva <sevaprilukov@mail.ru>
2023-05-25 10:54:53 +0300

    seva

--- 6/6.js
+++ 6/6.js
@@ -1,25 +1,25 @@
 function toggleImageState(image) {
-  if (!image.classList.contains("selected")) {
-    image.classList.add("selected");
-    image.style.border = "2px solid purple";
-    image.setAttribute("data-deletable", "true");
+  if (!image.classList.contains("selected")) { // Проверяем, нет ли у изображения класса "selected"
+    image.classList.add("selected"); // Если класса нет, то добавляем его
+    image.style.border = "2px solid purple"; // Устанавливаем фиолетовую границу
+    image.setAttribute("data-deletable", "true"); // Устанавливаем атрибут "data-deletable" со значением "true" (можно удалить)
   } else {
-    image.style.border = "2px solid red";
-    image.setAttribute("data-deletable", "false");
+    image.style.border = "2px solid red"; // Если класс "selected" уже присутствует, устанавливаем красную границу
+    image.setAttribute("data-deletable", "false"); // Устанавливаем атрибут "data-deletable" со значением "false" (нельзя удалить)
   }
 }
 
 function deleteSelectedImages() {
-  var imageContainer = document.getElementById("imageContainer");
-  var selectedImages = imageContainer.getElementsByClassName("selected");
+  var imageContainer = document.getElementById("imageContainer"); // Получаем ссылку на контейнер изображений
+  var selectedImages = imageContainer.getElementsByClassName("selected"); // Получаем коллекцию элементов с классом "selected"
 
   // Проверяем, можно ли удалять каждое изображение перед его удалением
   for (var i = selectedImages.length - 1; i >= 0; i--) {
     var image = selectedImages[i];
-    var isDeletable = image.getAttribute("data-deletable");
+    var isDeletable = image.getAttribute("data-deletable"); // Получаем значение атрибута "data-deletable"
 
-    if (isDeletable === "true") {
-      image.remove();
+    if (isDeletable === "true") { // Если значение равно "true", то изображение можно удалить
+      image.remove(); // Удаляем изображение
     }
   }
 }
