67cf79fbc219d7bea81a7785c6c5f7652d785b7d
Seva <sevaprilukov@mail.ru>
2023-05-25 10:05:56 +0300

    seva

--- 6/6.js
+++ 6/6.js
@@ -1,11 +1,11 @@
-function toggleImageState(image) {
-    image.classList.toggle("selected");
+function toggleImageState(image) {  //принимает параметр image, который представляет собой элемент изображения
+    image.classList.toggle("selected"); //предоставляет доступ к классам элемента а метод toggle() добавляет класс, если его нет, и удаляет класс, если он уже присутствует
   }
 
-  function deleteSelectedImages() {
-    var imageContainer = document.getElementById("imageContainer");
-    var selectedImages = imageContainer.getElementsByClassName("selected");
-    while (selectedImages.length > 0) {
-      selectedImages[0].remove();
+  function deleteSelectedImages() { //Функция deleteSelectedImages() не принимает параметров. Она начинается с получения элемента контейнера изображений с помощью document.getElementById("imageContainer")
+    var imageContainer = document.getElementById("imageContainer"); //получается ссылка на элемент контейнера изображений с помощью функции document.getElementById("imageContainer")
+    var selectedImages = imageContainer.getElementsByClassName("selected"); //создается переменная selectedImages, которая содержит коллекцию элементов с классом "selected"
+    while (selectedImages.length > 0) { //Внутри цикла while берется первый элемент из коллекции selectedImages с помощью индекса [0].
+      selectedImages[0].remove(); //Цикл продолжается, и на каждой итерации удаляется следующий выбранный элемент из коллекции, пока в коллекции selectedImages не останется элементов.
     }
   }
