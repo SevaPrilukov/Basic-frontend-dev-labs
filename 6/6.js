6e90e1f172da499f772d188f41b9c0902ef0689a
Seva <sevaprilukov@mail.ru>
2023-05-25 10:34:21 +0300

    seva

--- 6/6.js
+++ 6/6.js
@@ -1,4 +1,4 @@
-function toggleImageState(image) {  //принимает параметр image, который представляет собой элемент изображения
+/*function toggleImageState(image) {  //принимает параметр image, который представляет собой элемент изображения
     image.classList.toggle("selected"); //предоставляет доступ к классам элемента а метод toggle() добавляет класс, если его нет, и удаляет класс, если он уже присутствует
   }
 
@@ -9,3 +9,20 @@ function toggleImageState(image) {  //принимает параметр image,
       selectedImages[0].remove(); //Цикл продолжается, и на каждой итерации удаляется следующий выбранный элемент из коллекции, пока в коллекции selectedImages не останется элементов.
     }
   }
+*/
+function toggleImageState(image) {
+  if (!image.classList.contains("selected")) {
+    image.classList.add("selected");
+    image.style.border = "6px solid purple";
+  } else {
+    image.style.border = "6px solid red";
+  }
+}
+
+function deleteSelectedImages() {
+  var imageContainer = document.getElementById("imageContainer");
+  var selectedImages = imageContainer.getElementsByClassName("selected");
+  while (selectedImages.length > 0) {
+    selectedImages[0].remove();
+  }
+}
