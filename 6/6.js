4b054870adb13a5208591bcb03e04fca8bdc4ad4
Seva <sevaprilukov@mail.ru>
2023-05-25 10:42:26 +0300

    seva

--- 6/6.js
+++ 6/6.js
@@ -1,28 +1,25 @@
-/*function toggleImageState(image) {  //принимает параметр image, который представляет собой элемент изображения
-    image.classList.toggle("selected"); //предоставляет доступ к классам элемента а метод toggle() добавляет класс, если его нет, и удаляет класс, если он уже присутствует
-  }
-
-  function deleteSelectedImages() { //Функция deleteSelectedImages() не принимает параметров. Она начинается с получения элемента контейнера изображений с помощью document.getElementById("imageContainer")
-    var imageContainer = document.getElementById("imageContainer"); //получается ссылка на элемент контейнера изображений с помощью функции document.getElementById("imageContainer")
-    var selectedImages = imageContainer.getElementsByClassName("selected"); //создается переменная selectedImages, которая содержит коллекцию элементов с классом "selected"
-    while (selectedImages.length > 0) { //Внутри цикла while берется первый элемент из коллекции selectedImages с помощью индекса [0].
-      selectedImages[0].remove(); //Цикл продолжается, и на каждой итерации удаляется следующий выбранный элемент из коллекции, пока в коллекции selectedImages не останется элементов.
-    }
-  }
-*/
 function toggleImageState(image) {
   if (!image.classList.contains("selected")) {
     image.classList.add("selected");
-    image.style.border = "6px solid purple";
+    image.style.border = "2px solid purple";
+    image.setAttribute("data-deletable", "true");
   } else {
-    image.style.border = "6px solid red";
+    image.style.border = "2px solid red";
+    image.setAttribute("data-deletable", "false");
   }
 }
 
 function deleteSelectedImages() {
   var imageContainer = document.getElementById("imageContainer");
   var selectedImages = imageContainer.getElementsByClassName("selected");
-  while (selectedImages.length > 0) {
-    selectedImages[0].remove();
+
+  // Проверяем, можно ли удалять каждое изображение перед его удалением
+  for (var i = selectedImages.length - 1; i >= 0; i--) {
+    var image = selectedImages[i];
+    var isDeletable = image.getAttribute("data-deletable");
+
+    if (isDeletable === "true") {
+      image.remove();
+    }
   }
 }
